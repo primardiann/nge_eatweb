fda2b0cbfc9bd526b1a36264cf6dd83571c64ded
primardiann <ardiansyahprima2005@gmail.com>
2025-06-02 13:47:09 +0700

    Add project files.

---
+++ nge_eatweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace nge_eatweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
